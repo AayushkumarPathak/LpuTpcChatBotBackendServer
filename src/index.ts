068583006e2434e9c9e386b98d5e2e6d9011cb0a
aayushkumarpathak <aayushkumarpathak1@gmail.com>
2026-01-23 21:46:44 +0530

    Add frotend url to allow cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 // Enable CORS for frontend origin
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://tpc-chatbot-lpu.vercel.app',
   credentials: true
 }));
 app.use(express.json());
